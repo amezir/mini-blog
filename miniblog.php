d5723600f4e015fabb5d33d32926bcc1ee3ff00a
amezir <amezirmessaoud.pro@gmail.com>
2022-12-05 10:14:57 +0100

    change badge

--- miniblog.php
+++ miniblog.php
@@ -72,7 +72,7 @@ echo "<h1 class='text-center'>Les 3 dernier articles:</h1>";
                     <rect width='100%' height='100%' fill='#434e58'></rect>
                     <text x='50%' y='50%' fill='#f9f9f9' dy='.3em'></text>
                   </svg>
-                  <div class='card-img-overlay'><span class='badge badge-warning text-uppercase'>Articles</span></div>
+                  <div class='card-img-overlay'><span class='badge badge-warning text-uppercase'>New</span></div>
                 </div>
                 <div class='card-body bg-muted'>
                   <h5 class='card-title'>{$artciles["articles_titre"]}</h5>
